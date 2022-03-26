e5d70e4916278b88932219a83a00882aa5a05b7f
kokotr <antonioperezap077@gmail.com>
2022-03-25 20:31:44 -0600

    se modifico se realizo una suma en programacion en c

---
+++ texto2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+float a,b;
+
+
+printf("Dame un numero?:");
+scanf("%g",&a);
+
+
+printf("Dame otro numero?:");
+scanf("%g",&b);
+
+
+printf("el resultado de la suma es %d",a+b);
+
+
+
+}
+
+ 
+
